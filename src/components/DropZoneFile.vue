48e7f93730e1a4ddd796699d00a55054caf5c99d
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-12-20 19:23:30 +0100

    Create function to read file from drag and drop

--- src/components/DropZoneFile.vue
+++ src/components/DropZoneFile.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { e } from "vitest/dist/index-5aad25c1";
 import { ref } from "vue";
 
 const isActiveZone = ref(false);
 
 function preventEvent(e: Event) {
+    e.stopPropagation();
     e.preventDefault();
 }
 
@@ -15,18 +17,47 @@ function turnOffActiveZone() {
     isActiveZone.value = false;
 }
 
-function handleDrop(event: any) {}
+function onChangeInput(event: any) {
+    function onReaderLoad(event: any) {
+        const obj = JSON.parse(event.target.result);
+        console.log(obj);
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+}
+
+function handleDropFile(event: DragEvent) {
+    const files = event.dataTransfer!.files;
+
+    const jsonFile = files[0];
+    const reader = new FileReader();
+
+    reader.onload = (() => {
+        return () => {
+            const dataJson = JSON.parse(reader.result as string);
+
+            console.log(dataJson);
+        };
+    })();
+
+    reader.readAsText(jsonFile);
+}
 </script>
 
 <template>
-    <div
+    <input
+        type="file"
         class="zone"
         @dragenter="preventEvent($event), turnOnActiveZone()"
         @dragover="preventEvent($event), turnOnActiveZone()"
         @dragleave="preventEvent($event), turnOffActiveZone()"
-        @drop="preventEvent($event), turnOffActiveZone(), handleDrop($event)"
+        @drop="preventEvent($event), turnOffActiveZone(), handleDropFile($event)"
         :class="{ active: isActiveZone }"
-    ></div>
+        @change="onChangeInput($event)"
+    />
 </template>
 
 <style scoped lang="scss">
