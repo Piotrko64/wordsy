b8991e7a89535182cb5370683c639426052c184a
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-03 21:16:46 +0100

    Add function to turn on fullscreen mode

--- src/components/learningPage/modes/screensaverMode/ScreenSaverMode.vue
+++ src/components/learningPage/modes/screensaverMode/ScreenSaverMode.vue
@@ -1,6 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWordsStore } from "../../../../stores/WordsStore";
+import { ref, watch } from "vue";
+import { storeToRefs } from "pinia";
+
+const isScreensaverMode = ref(false);
+const elementToFullScreen = ref();
+const store = useWordsStore();
+const { getActualWord } = storeToRefs(store);
+
+function toggleFullScreen() {
+    if (elementToFullScreen.value.requestFullscreen) {
+        console.log(isScreensaverMode.value);
+        if (!isScreensaverMode.value) {
+            elementToFullScreen.value.requestFullscreen();
+            isScreensaverMode.value = true;
+        } else {
+            document.exitFullscreen();
+            isScreensaverMode.value = false;
+        }
+    }
+}
+
+watch(isScreensaverMode, () => {});
+</script>
 
 <template>
-    <div>a</div>
+    <div class="actionButton" @click="toggleFullScreen()">Włącz tryb Wyświetlacza</div>
+    <div ref="elementToFullScreen">
+        {{ getActualWord.exampleEN }}
+
+        <div class="actionButton" @click="toggleFullScreen()">Włącz tryb Wyświetlacza</div>
+    </div>
 </template>
 <style lang="scss"></style>
