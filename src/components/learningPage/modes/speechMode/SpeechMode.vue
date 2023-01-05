928586b3dbe365c9ffa3b6d017c1f17b1707b2fb
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-05 20:57:55 +0100

    Add checking words validity for speeching mode

--- src/components/learningPage/modes/speechMode/SpeechMode.vue
+++ src/components/learningPage/modes/speechMode/SpeechMode.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useWordsStore } from "../../../../stores/WordsStore";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import { storeToRefs } from "pinia";
 
 const resultSpeech = ref("");
-const { getActualWord } = useWordsStore();
+const isPermission = ref(false);
+const { getActualWord } = storeToRefs(useWordsStore());
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -13,8 +15,20 @@ function recognitionStart() {
 }
 
 onMounted(() => {
+    navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+            console.log(stream);
+            isPermission.value = true;
+        })
+        .catch((err) => {
+            console.error(`you got an error: ${err}`);
+            isPermission.value = false;
+        });
+
     console.log(!!SpeechRecognition);
     recognition.interimResults = true;
+    recognition.lang = "en-US";
 
     recognition.addEventListener("result", (e: SpeechRecognitionEvent) => {
         const yourSpeech = e.results[0][0].transcript;
@@ -22,13 +36,33 @@ onMounted(() => {
         console.log(!!recognition);
     });
 });
+
+watch(getActualWord, () => {
+    resultSpeech.value = "";
+});
 </script>
 
 <template>
     <h2>Tryb mówienia</h2>
+    <p v-if="!isPermission">Ten tryb potrzebuje pozwolenia na używanie mikrofonu!</p>
     {{ getActualWord.exampleEN }}
     <button @click="recognitionStart()">rere</button>
-    {{ resultSpeech }}
+    <div v-for="(word, index) in resultSpeech.split(' ')">
+        <span
+            :style="{
+                backgroundColor:
+                    word.toLowerCase() ===
+                    getActualWord.exampleEN
+                        .split(' ')
+                        [index].toLowerCase()
+                        .replace(/[^a-zA-Z0-9 | ']/g, '')
+                        ? 'var(--green)'
+                        : 'red',
+            }"
+        >
+            {{ word }}
+        </span>
+    </div>
 </template>
 
 <style scoped lang="scss"></style>
