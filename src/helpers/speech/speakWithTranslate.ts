a872c30ceaca09f64c0a3f7ecf140da1219a41eb
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-27 19:36:33 +0100

    🐞 fix: Problem with speaking in screensaver mode

--- src/helpers/speech/speakWithTranslate.ts
+++ src/helpers/speech/speakWithTranslate.ts
@@ -5,7 +5,9 @@ const TIME_TO_SPEAK = 600;
 export function speakWithTranslate(plWord: string, enWord: string) {
    speak(plWord, 'pl-PL', () => {
       setTimeout(() => {
-         speak(enWord, 'en-US');
+         if (!window.screenTop && !window.screenY) {
+            speak(enWord, 'en-US');
+         }
       }, TIME_TO_SPEAK);
    });
 }
