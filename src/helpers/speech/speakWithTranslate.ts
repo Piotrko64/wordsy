74692945080d8ada95f14b7df0fee943a6ceea24
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-27 20:09:26 +0100

    🐞 fix: Functions to speak

--- src/helpers/speech/speakWithTranslate.ts
+++ src/helpers/speech/speakWithTranslate.ts
@@ -5,7 +5,7 @@ const TIME_TO_SPEAK = 600;
 export function speakWithTranslate(plWord: string, enWord: string) {
    speak(plWord, 'pl-PL', () => {
       setTimeout(() => {
-         if (!window.screenTop && !window.screenY) {
+         if (document.fullscreenElement) {
             speak(enWord, 'en-US');
          }
       }, TIME_TO_SPEAK);
