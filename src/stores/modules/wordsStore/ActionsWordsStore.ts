5481d58d226366b45e67d72d2ce127ab4eff19f2
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-28 15:02:50 +0100

    Try find bug on deploy

--- src/stores/modules/wordsStore/ActionsWordsStore.ts
+++ src/stores/modules/wordsStore/ActionsWordsStore.ts
@@ -55,6 +55,8 @@ export const ActionsWordsStore = {
    },
 
    addWordAsFavourite(id: string) {
+      this.allWords = [...this.ownWords, this.startWords];
+
       const findIndex = this.allWords.findIndex((example) => example.id === id);
       console.log(this.allWords, id);
       if (findIndex === -1) return;
