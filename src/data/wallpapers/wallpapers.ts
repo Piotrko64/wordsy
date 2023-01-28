72c7a9e5fa0a5a7bb5d1f88c69613ea88a0515c2
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-28 21:31:43 +0100

    ✨feature: Add new wallpapers

--- src/data/wallpapers/wallpapers.ts
+++ src/data/wallpapers/wallpapers.ts
@@ -1,43 +1,57 @@
 const wallpapersData = [
-    {
-        name: "road",
-        hours: 19,
-    },
+   {
+      name: 'leaves',
+      hours: 23,
+   },
+   {
+      name: 'stars2',
+      hours: 22,
+   },
+   {
+      name: 'cat',
+      hours: 20,
+   },
+   {
+      name: 'road',
+      hours: 18,
+   },
 
-    {
-        name: "forest",
-        hours: 16,
-    },
-    {
-        name: "books",
-        hours: 13,
-    },
-    {
-        name: "mountain",
-        hours: 11,
-    },
-    {
-        name: "stars",
-        hours: 10,
-    },
-    {
-        name: "road",
-        hours: 7,
-    },
-    {
-        name: "mountain",
-        hours: 4,
-    },
-    {
-        name: "books",
-        hours: 0,
-    },
+   {
+      name: 'forest',
+      hours: 16,
+   },
+   {
+      name: 'books',
+      hours: 13,
+   },
+   {
+      name: 'mountain',
+      hours: 11,
+   },
+   {
+      name: 'stars',
+      hours: 10,
+   },
+   {
+      name: 'road',
+      hours: 7,
+   },
+   {
+      name: 'mountain',
+      hours: 4,
+   },
+   {
+      name: 'books',
+      hours: 1,
+   },
 ];
 
 export function findWallpaper() {
-    const hours = new Date().getHours();
+   const hours = new Date().getHours();
 
-    const nameImg = wallpapersData.find((wallpaper) => hours >= wallpaper.hours)?.name;
+   const nameImg = wallpapersData.find(
+      (wallpaper) => hours >= wallpaper.hours
+   )?.name;
 
-    return nameImg;
+   return nameImg;
 }
