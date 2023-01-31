11555ec96636e90f4d16d8f0ba5ff6e621bf8a22
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-01-31 10:53:56 +0100

    ✨feature: Temporary delete speech mode

--- src/data/navigation/dataModesNavigation.ts
+++ src/data/navigation/dataModesNavigation.ts
@@ -1,38 +1,41 @@
-import flashCardsImg from "../../assets/icons/modes/cards.png";
-import quizImg from "../../assets/icons/modes/quiz.png";
-import quizVoiceImg from "../../assets/icons/modes/voiceQuiz.png";
-import display from "../../assets/icons/modes/display.png";
-import micro from "../../assets/icons/modes/greenMicro.png";
+import flashCardsImg from '../../assets/icons/modes/cards.png';
+import quizImg from '../../assets/icons/modes/quiz.png';
+import quizVoiceImg from '../../assets/icons/modes/voiceQuiz.png';
+import display from '../../assets/icons/modes/display.png';
+import micro from '../../assets/icons/modes/greenMicro.png';
 
 export const dataModesNavigation = [
-    {
-        name: "Fiszki",
-        urlName: "fiszki",
-        img: flashCardsImg,
-    },
-    {
-        name: "Quiz",
-        urlName: "quiz",
-        img: quizImg,
-    },
-    {
-        name: "Głosowy Quiz",
-        urlName: "voiceQuiz",
-        img: quizVoiceImg,
-    },
-    {
-        name: "Tryb wygaszacza",
-        urlName: "trybWygaszacza",
-        img: display,
-    },
-    {
-        name: "Wymów zdanie",
-        urlName: "trybSpeech",
-        img: micro,
-        notice: "Tylko na Chrome (PC)",
-    },
+   {
+      name: 'Fiszki',
+      urlName: 'fiszki',
+      img: flashCardsImg,
+   },
+   {
+      name: 'Quiz',
+      urlName: 'quiz',
+      img: quizImg,
+   },
+   {
+      name: 'Głosowy Quiz',
+      urlName: 'voiceQuiz',
+      img: quizVoiceImg,
+   },
+   {
+      name: 'Tryb wygaszacza',
+      urlName: 'trybWygaszacza',
+      img: display,
+   },
+   // {
+   //     name: "Wymów zdanie",
+   //     urlName: "trybSpeech",
+   //     img: micro,
+   //     notice: "Tylko na Chrome (PC)",
+   // },
 ];
 
 export function findRouteName(name: string) {
-    return dataModesNavigation.find((mode) => mode.urlName === name)?.name || "Fiszki";
+   return (
+      dataModesNavigation.find((mode) => mode.urlName === name)?.name ||
+      'Fiszki'
+   );
 }
