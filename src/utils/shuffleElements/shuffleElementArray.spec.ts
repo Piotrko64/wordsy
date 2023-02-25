63232e5a7ad9438c9586e1697a4805d799a61714
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-02-25 13:38:43 +0100

    Test husky

--- src/utils/shuffleElements/shuffleElementArray.spec.ts
+++ src/utils/shuffleElements/shuffleElementArray.spec.ts
@@ -1,25 +1,25 @@
-import { fakeStartingWordsData } from "../../data/test/fakeStartingWordsData";
-import { describe, it, expect } from "vitest";
-import { shuffleElementsArray } from "./shuffleElementsArray";
+import { fakeStartingWordsData } from '../../data/test/fakeStartingWordsData';
+import { describe, it, expect } from 'vitest';
+import { shuffleElementsArray } from './shuffleElementsArray';
 
-describe("checking function to shuffle elements in array ", () => {
+describe('checking function to shuffle elements in array ', () => {
     const exampleArray = [0, 1, 2];
 
-    it("arrays should be equal", () => {
+    it('arrays should be equal', () => {
         expect(exampleArray).toEqual([0, 1, 2]);
     });
 
-    it("arrays should be different", () => {
+    it('arrays should be different', () => {
         expect(exampleArray).not.toEqual([0]);
     });
 
-    it("arrays should be different", () => {
+    it('arrays should be different', () => {
         const shuffleArray = shuffleElementsArray([...exampleArray]);
 
-        expect(shuffleArray).not.toEqual(exampleArray);
+        expect(shuffleArray).not.toBe(exampleArray);
     });
 
-    it("arrays should be different - example with fake words", () => {
+    it('arrays should be different - example with fake words', () => {
         const shuffleArray = shuffleElementsArray([...fakeStartingWordsData]);
 
         expect(shuffleArray).not.toEqual(fakeStartingWordsData);
